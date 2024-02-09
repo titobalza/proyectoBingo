e5ef036ac60ae528375b9914d1dfc1ba9d7f67f3
titobalza <titobalzaromero@gmail.com>
2024-02-09 02:31:00 +0000

    boton funcional

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const mainPage = document.getElementById("mainPage");
+const startButton = document.getElementById("start");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const divMenu =
+  '<div><h2>Ingrese los nombres de los usuarios:</h2><label>Jugador 1:</label><input type="text" id="jugador1" /><label>Jugador 2:</label><input type="text" id="jugador2" /><label>Jugador 3:</label><input type="text" id="jugador3" /><label>Jugador 4:</label><input type="text" id="jugador4" /></div>';
+
+function initialize() {
+  mainPage.innerHTML = divMenu;
+  startButton.style.display = "none"; // Hide the start button after initialization
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  startButton.style.display = "block"; // Show the start button on page load
 };
+
+startButton.addEventListener("click", initialize);
